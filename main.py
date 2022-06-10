5ff61ed4de7e5687c999e438d1e6e74542baf744
Katherina <kcortes@ucsd.edu>
2022-06-10 09:24:16 -0600

    init

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 from src import dataExploration, database, alignment
 
+from collections import Counter
+
 
 annotationFile = 'Outputs/SRR12464727_Annotation.csv'
 testF = 'Inputs/SRR12464727.fastq'
@@ -39,6 +41,7 @@ if kmerBinning:
     s1 = time.time()
     # TODO: make dictionary {read: {viralGenome:#hits, viralGenome:hits}, ....}
     hits = {}
+    totHits = {}
     for r in reads:
         possibleVGs = []
         for i in range(kLen, len(r)-kLen):
@@ -46,21 +49,27 @@ if kmerBinning:
             if k in kmers:
                 possibleVGs.extend(kmers[k])
         hits[count] = len(possibleVGs)
+        totHits = Counter(possibleVGs)
         #if count%10000==0: print(count)
         count +=1
     print(time.time()-s)
     print(time.time()-s1)
     top = dict(sorted(hits.items(), key=lambda item: item[1], reverse=True)[:10])
     # TODO : alignment for reads with most hits for viralGenomes they matched to
-    x = hits.values()
-    print(len(list(filter(lambda x: x>6000, sorted(x)))))
-    x2 = sorted(x, reverse=True)[:3000]
+    for t in top:
+        print(top[t], len(totHits[t]))
+        for align in range(3):
+            print(reads[t], totHits[t][align])
+
+    #x = hits.values()
+    #print(len(list(filter(lambda x: x>6000, sorted(x)))))
+    #x2 = sorted(x, reverse=True)[:3000]
 
     #plt.hist(x)
     #plt.show()
 
-    plt.hist(x2)
-    plt.show()
+    #plt.hist(x2)
+    #plt.show()
 
 
 
